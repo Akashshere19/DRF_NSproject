001df559acefa2701ab1fe39e0ed1d474b90385e
akashshere19 <akashshere19@gmail.com>
2023-01-18 22:31:11 +0530

    add authentication

--- NSapp/views.py
+++ NSapp/views.py
@@ -2,12 +2,29 @@ from django.shortcuts import render
 from .models import Course,Instructor
 from rest_framework import generics
 from .serializers import InstructorSerializer,CourseSerializer
+from rest_framework.permissions import IsAuthenticated,IsAdminUser,BasePermission
+from rest_framework.authentication import BasicAuthentication
 # Create your views here.
+
+class WriteAdminOnlyPermisions(BasePermission):
+     def has_permission(self,request,view):
+        user = request.user
+        if request.method == 'GET':
+          return True
+        if request.method == 'POST' or request.method == 'PUT' or  request.method == 'DELETE':
+            if user.is_superuser:
+                return True   
+        return False
+
 class InstructorListView(generics.ListCreateAPIView):
+    authentications_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     serializer_class = InstructorSerializer
     queryset = Instructor.objects.all()
 
 class CourseListView(generics.ListCreateAPIView):
+    authentications_classes = [IsAuthenticated,BasicAuthentication]
+    permission_classes = [WriteAdminOnlyPermisions]
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
 
